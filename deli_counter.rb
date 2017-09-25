2a8c7b1938374c49b9bc1bdd1293a8b13d01fed6
Adam Fudge <coolfudge@gmail.com>
2017-09-25 20:01:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,14 @@ def line(array)
   if array.size == 0
     return "The line is currently empty"
   else
+    output = 
     # needs code
   end
 end
 
 def take_a_number(array, name)
   array << name
-  output = "Welcome, #{name}. You are number #{array.size} in line."
+  "Welcome, #{name}. You are number #{array.size} in line."
 end
 
 def now_serving(array)
