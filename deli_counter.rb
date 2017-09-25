8480196509003a3f227bfaeac60ba3658a04ac99
Adam Fudge <coolfudge@gmail.com>
2017-09-25 19:58:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,21 @@
 katz_deli = []
 
+def line(array)
+  if array.size == 0
+    return "The line is currently empty"
+  else
+    # needs code
+  end
+end
+
+def take_a_number(array, name)
+  
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts ""
+  end
+end
